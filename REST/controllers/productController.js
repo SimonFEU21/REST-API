891a9eae1c538c19f681fed9e9ffb5f0af3597ee
SimonFEU21 <simon.habchi@utb.ecutbildning.se>
2022-03-02 17:44:25 +0100

    fixat pModel samt pSchema

--- REST/controllers/productController.js
+++ REST/controllers/productController.js
@@ -1,6 +1,20 @@
 const router = require('express').Router();
+const productModel = require('../models/products/productModel');
 
+// // Få alla produkter
+// router.get('/',)
 
+// // Få en produkt via ID
+// router.get('/:id')
+
+// //Skapa en ny produkt
+router.post('/', productModel.createProduct);
+
+// //Uppdatera product
+// router.patch('/:id')
+
+// //Radera en produkt
+// router.delete('/:id')
 
 
 module.exports = router;
---
+++ REST/models/products/productModel.js
@@ -0,0 +1,46 @@
+const Product = require('./productSchema');
+
+
+exports.createProduct = (req, res) => {
+    Product.exists({ name: req.body.namn }, (err, result) => {
+
+        if(err) {
+            return res.status(500).json(err)
+        }
+
+        if(result) {
+            return res.status(400).json({
+                statusCode: 400,
+                status: false,
+                message: 'A product by that name alreade exists, please updtade product instead'
+            })
+        }
+
+        
+        Product.create({
+            name:   req.body.name,
+            short:  req.body.short,
+            desc:   req.body.desc,
+            price:  req.body.price,
+            image:  req.body.image,
+        })
+
+        .then(data => {
+            res.status(201).json({
+                statusCode: 201,
+                status: true,
+                message: 'Product created successfully',
+                data
+            })
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                statusCode: 500,
+                status: false,
+                message: 'failed to create prudct',
+                err 
+            })
+        })
+    })
+}
